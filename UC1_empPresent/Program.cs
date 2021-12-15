ef9b2d3a5ad66e7ededa18a9361507b85f4060c7
hkvijay54 <hkvijay54@gmail.com>
2021-12-15 17:36:01 +0530

    UC-2 Program

--- UC1_empPresent/Program.cs
+++ UC1_empPresent/Program.cs
@@ -1,21 +1,29 @@
 ﻿using System;
-// check employee pesent or not
+// check employee Wage
 class Program
 {
     static void Main(string[] args)
     {
         //constants
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        // Variables
+        int emphrs = 0;
+        int empwage = 0;
+
         Random random = new Random();
 
         int empCheck = random.Next(0, 2);
         if (empCheck == IS_FULL_TIME)
         {
-            Console.WriteLine("Employee is Present = " + empCheck);
+            emphrs = 8;
         }
         else
         {
-            Console.WriteLine("Employee is absent = " + empCheck);
+            emphrs = 0;
         }
+        empwage = emphrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("emp Wage: " + empwage);
     }
 }
