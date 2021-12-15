4871ff301e126b622a111c4d4c09dfb75115a1b8
hkvijay54 <hkvijay54@gmail.com>
2021-12-15 19:51:03 +0530

    UC-4 code changes

--- UC1_empPresent/Program.cs
+++ UC1_empPresent/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         //constants
-        //int IS_FULL_TIME = 1;
+        const int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
-        //int IS_PART_TIME = 2;
+        const int IS_PART_TIME = 2;
 
         // Variables
         int emphrs = 0;
@@ -19,11 +19,11 @@ class Program
 
         switch (empCheck)
         {
-            case 1:
+            case IS_FULL_TIME:
                 Console.WriteLine("Employee is full time");
                 emphrs = 8;
                 break;
-            case 2 :
+            case IS_PART_TIME :
                 Console.WriteLine("Employee is part time");
                 emphrs = 4;
                 break;
