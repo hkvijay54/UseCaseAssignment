aff9486b16d281970a2f4c6e771072633eab9281
hkvijay54 <hkvijay54@gmail.com>
2021-12-15 17:06:54 +0530

    first commit

---
+++ UC1_empPresent/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// check employee pesent or not
+class Program
+{
+    static void Main(string[] args)
+    {
+        //constants
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present = " + empCheck);
+        }
+        else
+        {
+            Console.WriteLine("Employee is absent = " + empCheck);
+        }
+    }
+}
